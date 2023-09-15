29910e0d40a18bcd0f2a7562794e0176cb001b63
Eva Bartolome <eva.bartolome@mrjaffesclass.com>
2023-09-15 11:30:02 -0700

    Commit message

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -8,6 +8,7 @@
 public class RenameThisClass
 {
     public static void main(String[] args) {
+        String[]strArray = {"Alpha","Bravo","Charlie","Delta","Echo"};
         System.out.println("Hello World!");
     }
 }
