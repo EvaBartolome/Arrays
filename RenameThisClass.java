ce7a94c7ca79fe6dc669370a1d4f581a5b109c65
EvaBartolome <eva.bartolome@mrjaffesclass.com>
2023-09-17 18:39:03 -0700

    Commit message

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -9,17 +9,27 @@ public class RenameThisClass
 {
     public static void main(String[] args) {
         String[]strArray = {"Alpha","Bravo","Charlie","Delta","Echo"}; //Declaring Array
-        for (int i = 0; i< strArray.length; i++) {
+        
+        //Separate lines
+        for (int i = 0; i < strArray.length; i++) //loop counter, loop condition,following sequence
+        {
         System.out.println(strArray[i]);    
         }
+        
         //Same line
-        for (String item : strArray) {
+        for (String item : strArray) 
+        {
             System.out.print(item + " ");
         }
-         //Separated by a dash
-        for (int i = 0; i < strArray.length; i++) {
+        
+        System.out.println(); //separate 
+        
+        //Separated by a dash
+        for (int i = 0; i < strArray.length; i++) 
+        {
             System.out.print(strArray[i]);
-            if (i < strArray.length - 1) {
+            if (i < strArray.length - 1) 
+            {
                 System.out.print(" - ");
             }
         }
